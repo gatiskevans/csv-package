6549d9192f229ad3fdfc12b3d7c09f27ccb3a2f7
Gatis Ķevans <gatis.kevans@gmail.com>
2021-09-22 20:57:03 +0300

    Added search functionality for countries

---
+++ app/Search.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace App;
+
+    class Search
+    {
+
+        public function search(string $element, ?string $search): bool
+        {
+            return $element === $search;
+        }
+
+        public function checkIfIsset(string $element): void
+        {
+            if(!isset($_GET[$element])) $_GET[$element] = '';
+        }
+
+    }
--- index.php
+++ index.php
@@ -3,8 +3,11 @@
     require_once 'vendor/autoload.php';
 
     use App\Statistics;
+    use App\Search;
 
     $covidStatisticRecords = new Statistics('app/Data/data.csv');
+    $search = new Search();
+    $search->checkIfIsset('search');
 
 ?>
 
@@ -18,6 +21,13 @@
     <title>Covid Statistics</title>
 </head>
 <body>
+
+    <form method="get">
+        <label for="search">Search</label>
+        <input type="text" name="search" id="search">
+        <input type="submit" name="submit">
+    </form>
+
     <table class="table">
         <tbody>
         <?php foreach($covidStatisticRecords->getHeader() as $header): ?>
@@ -28,22 +38,28 @@
         <?php endforeach; ?>
 
         <?php foreach($covidStatisticRecords->getStatement() as $record): ?>
+            <?php if($search->search($record['Valsts'], $_GET['search']) ||
+                $_GET['search'] === ''): ?>
+
             <tr>
-                <td><?= $record['Datums'] ?></td>
-                <td><?= $record['Valsts'] ?></td>
-                <td><?= $record['14DienuKumulativaIncidence'] ?></td>
-                <td><?= $record['Izcelosana'] ?></td>
-                <td><?= $record['Pasizolacija'] ?></td>
-                <td><?= $record['PersIrVakcParslSert_PasizolacijaLatvija'] ?></td>
-                <td><?= $record['PersIrVakcParslSert_TestsPirmsIebrauksanasLV'] ?></td>
-                <td><?= $record['PersIrVakcParslSert_TestsPecIebrauksanasLV'] ?></td>
-                <td><?= $record['CitasPersonas_PasizolacijaLatvija'] ?></td>
-                <td><?= $record['CitasPersonas_TestsPirmsIebrauksanasLV'] ?></td>
-                <td><?= $record['CitasPersonas_TestsPecIebrauksanasLV'] ?></td>
+                    <td><?= $record['Datums'] ?></td>
+                    <td><?= $record['Valsts'] ?></td>
+                    <td><?= $record['14DienuKumulativaIncidence'] ?></td>
+                    <td><?= $record['Izcelosana'] ?></td>
+                    <td><?= $record['Pasizolacija'] ?></td>
+                    <td><?= $record['PersIrVakcParslSert_PasizolacijaLatvija'] ?></td>
+                    <td><?= $record['PersIrVakcParslSert_TestsPirmsIebrauksanasLV'] ?></td>
+                    <td><?= $record['PersIrVakcParslSert_TestsPecIebrauksanasLV'] ?></td>
+                    <td><?= $record['CitasPersonas_PasizolacijaLatvija'] ?></td>
+                    <td><?= $record['CitasPersonas_TestsPirmsIebrauksanasLV'] ?></td>
+                    <td><?= $record['CitasPersonas_TestsPecIebrauksanasLV'] ?></td>
             </tr>
+
+            <?php endif; ?>
         <?php endforeach; ?>
 
         </tbody>
     </table>
+
 </body>
 </html>
