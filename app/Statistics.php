e5a7fb401e132597da9c39933573f341ae679a21
Gatis Ķevans <gatis.kevans@gmail.com>
2021-09-22 19:51:13 +0300

    CSV table using League/csv package

---
+++ app/Statistics.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace App;
+    use League\Csv\Reader;
+    use League\Csv\Statement;
+    use League\Csv\TabularDataReader;
+
+    class Statistics
+    {
+        private Reader $statistics;
+
+        public function __construct(string $path)
+        {
+            $this->statistics = Reader::createFromPath($path, 'r');
+            $this->statistics->setHeaderOffset(0);
+            $this->statistics->setDelimiter(';');
+        }
+
+        public function getHeader(): array
+        {
+            return $this->statistics->getHeader();
+        }
+
+        public function getStatement(): TabularDataReader
+        {
+            return Statement::create()->process($this->statistics);
+        }
+    }
--- index.php
+++ index.php
@@ -1,36 +1,49 @@
 <?php
 
     require_once 'vendor/autoload.php';
-    use App\Data;
 
-    $data = new Data();
-    $data->dataLoader();
-    $data->explodeHeader();
-    $data->explodeRecords();
+    use App\Statistics;
+
+    $covidStatisticRecords = new Statistics('app/Data/data.csv');
 
 ?>
+
+<html lang="en">
+<head>
     <style>
         table, th, td {
-            border:1px solid black;
+            border: 1px solid black;
         }
     </style>
-    <table>
-        <tr>
-<?php
+    <title>Covid Statistics</title>
+</head>
+<body>
+    <table class="table">
+        <tbody>
+        <?php foreach($covidStatisticRecords->getHeader() as $header): ?>
 
-    foreach($data->explodeHeader() as $field){
-        echo "<td>$field</td>";
-    }
+            <?php if(strlen($header) > 20) $header = substr($header, 0, 17) . '...'; ?>
+            <td><?= $header ?></td>
 
-    echo "</tr>";
+        <?php endforeach; ?>
 
-    foreach($data->getRecords() as $records) {
-        echo "<tr>";
-        foreach($records as $record) {
-            echo "<td>$record</td>";
-        }
-        echo "</tr>";
-    }
+        <?php foreach($covidStatisticRecords->getStatement() as $record): ?>
+            <tr>
+                <td><?= $record['Datums'] ?></td>
+                <td><?= $record['Valsts'] ?></td>
+                <td><?= $record['14DienuKumulativaIncidence'] ?></td>
+                <td><?= $record['Izcelosana'] ?></td>
+                <td><?= $record['Pasizolacija'] ?></td>
+                <td><?= $record['PersIrVakcParslSert_PasizolacijaLatvija'] ?></td>
+                <td><?= $record['PersIrVakcParslSert_TestsPirmsIebrauksanasLV'] ?></td>
+                <td><?= $record['PersIrVakcParslSert_TestsPecIebrauksanasLV'] ?></td>
+                <td><?= $record['CitasPersonas_PasizolacijaLatvija'] ?></td>
+                <td><?= $record['CitasPersonas_TestsPirmsIebrauksanasLV'] ?></td>
+                <td><?= $record['CitasPersonas_TestsPecIebrauksanasLV'] ?></td>
+            </tr>
+        <?php endforeach; ?>
 
-?>
+        </tbody>
     </table>
+</body>
+</html>
